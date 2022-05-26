08cf8e9619152e62b69cfbfbe9e774d65d7e585c
liamageo <103076900+liamageo@users.noreply.github.com>
2022-05-26 16:43:11 +0000

    Adds world's smallest form handler

--- index.php
+++ index.php
@@ -1,16 +1,20 @@
 <html>
   <head>
-    <title>PHP Test Page</title>
+    <title>World's Smallest Form Handler</title>
   </head>
   <body>
-    <h1>PHP Test Page</h1>
-    <?php
-    echo '<p>This is PHP!</p>';
-    ?>
-    <p>Hello from Github</P>
-    <?php
-    echo '<p>This is a change from repl.it!</p>';
-    ?>
-    <p>This is a change from VSCODE</p>
+    <h1>World's Smallest Form Handler</h1>
+  <?php
+  if(isset($_POST["FirstName"])){// show data
+    echo $_POST["FirstName"];
+  }else{// show form
+    echo '
+        <form method="post" action="">
+        <p>First Name: <input type="text" name="FirstName" /></p>
+        <p><input type="submit" /></p>
+        </form>
+         ';
+  }
+  ?>    
   </body>
 </html>
